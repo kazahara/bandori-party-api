a6bb886604f213836b3e973da3d8f4ca9ce83195
Kazahara <kzhr.sama@gmail.com>
2017-07-18 18:20:00 +0900

    added parameters-to-values

---
+++ parameters-to-values.js
@@ -0,0 +1,47 @@
+module.exports = {
+	astrological_signs: astrological_signs,
+	attributes: attributes,
+	school_years: school_years,
+	skill_types: skill_types,
+	trainables: trainables
+}
+
+var astrological_signs = {
+	leo: 0,
+	aries: 1,
+	libra: 2,
+	virgo: 3,
+	scorpio: 4,
+	capricorn: 5,
+	pisces: 6,
+	gemini: 7,
+	cancer: 8,
+	sagittarius: 9,
+	aquarius: 10,
+	taurus: 11
+};
+
+var attributes = {
+	power: 1,
+	cool: 2,
+	pure: 3,
+	happy: 4
+};
+
+var school_years = {
+	first: 1,
+	second: 2,
+	third: 3
+};
+
+var skill_types = {
+	score_up: 1,
+	life_recovery: 2,
+	perfect_lock: 3
+}
+
+var trainables = {
+	both: 1,
+	trainable: 2,
+	not_trainable: 3
+};
