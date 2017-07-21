4d7b9535f926bcacf4bb0857914bcf981c617d28
_kazahara <dev.kzhr@gmail.com>
2017-07-21 20:34:59 +0900

    deprecate callback implemented ES6 promise

--- bandori.js
+++ bandori.js
@@ -7,65 +7,68 @@ var request = require('request');
 
 var bandori_api = 'http://bandori.party/api';
 
-function get(endpoint, parameters, callback) {
-	if (arguments.length == 2) {
-		callback = arguments[1];
-		parameters = undefined;
-	}
-
-	if (endpoint == '') {
-		throw new Error('endpoint cannot be an empty String');
-	}
-	if (typeof endpoint != 'string') {
-		throw new TypeError('endpoint is not a String');
-	}
-	if (typeof parameters != 'object' && typeof parameters != 'number' &&
-		parameters != undefined) {
-		throw new TypeError('parameters needs to be an Object or Number');
-	}
-	if (typeof callback != 'function') {
-		throw new TypeError('callback is not a Function');
-	}
-
-	if (typeof parameters == 'number') {
-		var id = parameters;
-		endpoint += `/${id}`;
-	}
-	var options = {
-		baseUrl: bandori_api,
-		url: endpoint,
-		method: `GET`,
-		json: true
-	};
-	if (typeof parameters == 'object') {
-		check_i_parameters(parameters);
-		options.qs = parameters;
-	}
-
-	request(options, (error, response, data) => {
-		if (response.statusCode == 404) {
-			error = `The requested URL "${endpoint}" was not found.`;
-			data = undefined;
+function get(endpoint, parameters) {
+	var promise = new Promise((resolve, reject) => {
+		if (endpoint == '') {
+			throw new Error('first argument of get() cannot be an empty String');
+		}
+		if (typeof endpoint != 'string') {
+			throw new TypeError('first argument of get() is not a String');
+		}
+		if (typeof parameters != 'object' && typeof parameters != 'number' &&
+			parameters != undefined) {
+			throw new TypeError('second argument of get() needs to be an Object or Number');
 		}
 
-		callback(error, data);
+		if (typeof parameters == 'number') {
+			var id = parameters;
+			endpoint += `/${id}`;
+		}
+		var options = {
+			baseUrl: bandori_api,
+			url: endpoint,
+			method: `GET`,
+			json: true
+		};
+		if (typeof parameters == 'object') {
+			check_i_parameters(parameters);
+			options.qs = parameters;
+		}
+
+		request(options, (error, response, data) => {
+			response.body = data;
+			resolve(response);
+		});
 	});
+	return promise;
 }
 
 function filter(data, parameters) {
-	if (typeof data != 'object') {
-		throw new TypeError('data is not an object');
-	}
+	var promise = new Promise((resolve, reject) => {
+		if (typeof data != 'object') {
+			throw new TypeError('first argument is not an object');
+		}
 
-	check_i_parameters(parameters);
+		check_i_parameters(parameters);
 
-	for (var parameter in parameters) {
-		var filter_value = parameters[parameter].toLowerCase();
-		data.results = data.results.filter((value, index, array) => {
-			var object_value = value[parameter].toLowerCase();
-			return (object_value == filter_value);
-		});
-	}
+		for (var parameter in parameters) {
+			var filter_value = parameters[parameter];
+			if (typeof filter_value == 'number') {
+				filter_value = filter_value.toString();
+			}
+			filter_value = filter_value.toLowerCase();
+			var filtered_data = data.results.filter((value, index, array) => {
+				var object_value = value[parameter];
+				if (typeof object_value == 'number') {
+					object_value = object_value.toString();
+				}
+				object_value = object_value.toLowerCase();
+				return (object_value == filter_value);
+			});
+		}
+		resolve(filtered_data);
+	});
+	return promise;
 }
 
 var i_parameters = {
@@ -81,7 +84,7 @@ var i_parameters = {
 
 function check_i_parameters(parameters) {
 	if (typeof parameters != 'object') {
-		throw new TypeError('parameters is not an object');
+		throw new TypeError('first argument is not an object');
 	}
 
 	for (var parameter in parameters) {
