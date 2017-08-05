618f85095a8a8d569461c1035f502501830bea99
_kazahara <dev.kzhr@gmail.com>
2017-08-05 22:32:51 +0900

    removed request package dependency

--- bandori.js
+++ bandori.js
@@ -1,38 +1,60 @@
-const request = require(`request`);
-
-const bandori_api = `http://bandori.party/api`;
+const http = require(`http`);
+const { URL, URLSearchParams } = require(`url`);
 
 class Bandori {
   get(endpoint, parameters) {
-    let promise = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       if (endpoint == ``) {
-        throw new Error(
-          `bandori.get(endpoint[, parameters]): endpoint cannot be an empty String`);
+        reject(`bandori.get(endpoint[, parameters]): endpoint cannot be an empty String`);
       }
       if (typeof endpoint != `string`) {
-        throw new TypeError(
-          `bandori.get(endpoint[, parameters]): endpoint is not a String`);
+        reject(`bandori.get(endpoint[, parameters]): endpoint is not a String`);
       }
       if (typeof parameters != `object` && typeof parameters != `number` &&
         parameters != undefined) {
-        throw new TypeError(
-          `bandori.get(endpoint[, parameters]): parameters needs to be an Object or a Number`);
+        reject(`bandori.get(endpoint[, parameters]): parameters needs to be an Object or a Number`);
       }
 
-      let options = {
-        method: `GET`,
-        json: true,
-        baseUrl: bandori_api,
-        url: (typeof parameters == `number`) ? `${endpoint}/${parameters}` : endpoint,
-        qs: (typeof parameters == `object`) ? parameters : undefined,
-      };
+      let url = new URL(`http://bandori.party`);
+      url.pathname = `/api/${endpoint}/`;
+      if (typeof parameters == `number`) {
+        const id = parameters;
+        url.pathname += `${id}/`;
+      } else if (typeof parameters == `object`) {
+        const queries = new URLSearchParams(parameters);
+        url.search = queries;
+      }
 
-      request(options, (error, response, data) => {
-        response.body = data;
-        resolve(response);
+      http.get(url, (response) => {
+        const { statusCode } = response;
+        const contentType = response.headers[`content-type`];
+
+        let error;
+        if (statusCode !== 200) {
+          error = new Error(`Request Failed. Status Code: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+          error = new Error(`Invalid content-type. Expected application/json but received ${contentType}`);
+        }
+        if (error) {
+          response.resume(); // consume response data to free up memory
+          reject(error.message);
+        }
+
+        response.setEncoding('utf8');
+        let raw_data = ``;
+        response.on(`data`, (chunk) => raw_data += chunk);
+        response.on(`end`, () => {
+          try {
+            const data = JSON.parse(raw_data);
+            resolve(data);
+          } catch (error) {
+            reject(error.message);
+          }
+        });
+      }).on(`error`, (error) => {
+        reject(error.message);
       });
     });
-    return promise;
   }
 }
 
