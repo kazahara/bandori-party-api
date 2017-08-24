f750368d9d0df691e68b28de6a3dd136f13a3240
_kazahara <dev.kzhr@gmail.com>
2017-08-24 19:40:27 +0900

    refactor to async

--- bandori.js
+++ bandori.js
@@ -1,61 +1,35 @@
-const http = require(`http`);
-const { URL, URLSearchParams } = require(`url`);
+const { getJSON } = require('./lib/http-promise.js')
+const { URL, URLSearchParams } = require('url')
 
 class Bandori {
-  get(endpoint, parameters) {
-    return new Promise((resolve, reject) => {
-      if (endpoint == ``) {
-        reject(`bandori.get(endpoint[, parameters]): endpoint cannot be an empty String`);
-      }
-      if (typeof endpoint != `string`) {
-        reject(`bandori.get(endpoint[, parameters]): endpoint is not a String`);
-      }
-      if (typeof parameters != `object` && typeof parameters != `number` &&
-        parameters != undefined) {
-        reject(`bandori.get(endpoint[, parameters]): parameters needs to be an Object or a Number`);
-      }
+  async get (endpoint, parameters) {
+    if (endpoint === '') {
+      throw 'bandori.get(endpoint[, parameters]): endpoint cannot be an empty String'
+    }
+    if (typeof endpoint !== 'string') {
+      'bandori.get(endpoint[, parameters]): endpoint is not a String'
+    }
+    if (typeof parameters !== 'object'
+     && typeof parameters !== 'number'
+     && parameters !== undefined) {
+      throw 'bandori.get(endpoint[, parameters]): parameters needs to be an Object or a Number'
+    }
 
-      let url = new URL(`http://bandori.party`);
-      url.pathname = `/api/${endpoint}/`;
-      if (typeof parameters == `number`) {
-        const id = parameters;
-        url.pathname += `${id}/`;
-      } else if (typeof parameters == `object`) {
-        const queries = new URLSearchParams(parameters);
-        url.search = queries;
-      }
+    let url = new URL(`http://bandori.party/`)
+    url.pathname = `api/${endpoint}/`
+    switch (typeof parameters) {
+      case 'number':
+        const id = parameters
+        url.pathname += `${id}/`
+        break
+      case 'object':
+        const queries = new URLSearchParams(parameters)
+        url.search = queries
+    }
 
-      http.get(url, (response) => {
-        const { statusCode } = response;
-        const contentType = response.headers[`content-type`];
-
-        let error;
-        if (statusCode !== 200) {
-          error = new Error(`Request Failed. Status Code: ${statusCode}`);
-        } else if (!/^application\/json/.test(contentType)) {
-          error = new Error(`Invalid content-type. Expected application/json but received ${contentType}`);
-        }
-        if (error) {
-          response.resume(); // consume response data to free up memory
-          reject(error.message);
-        }
-
-        response.setEncoding('utf8');
-        let raw_data = ``;
-        response.on(`data`, (chunk) => raw_data += chunk);
-        response.on(`end`, () => {
-          try {
-            const data = JSON.parse(raw_data);
-            resolve(data);
-          } catch (error) {
-            reject(error.message);
-          }
-        });
-      }).on(`error`, (error) => {
-        reject(error.message);
-      });
-    });
+    const data = await getJSON(url)
+    return data
   }
 }
 
-module.exports = new Bandori;
+module.exports = new Bandori()
